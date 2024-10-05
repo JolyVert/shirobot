6f96b737e561fb9b50a7e3e08a46e3955147d322
Anton Shkumat <nous927@gmail.com>
2024-10-05 12:59:50 +0200

    first commit

---
+++ src/main/java/com/project/TelegramBot/model/User.java
@@ -0,0 +1,65 @@
+package com.project.TelegramBot.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    private Long chatId;
+
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
+    @Column
+    private String username;
+    @Column
+    private Timestamp registeredAt;
+
+    public Long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(Long chatId) {
+        this.chatId = chatId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Timestamp getRegisteredAt() {
+        return registeredAt;
+    }
+
+    public void setRegisteredAt(Timestamp registeredAt) {
+        this.registeredAt = registeredAt;
+    }
+}
---
+++ src/main/java/com/project/TelegramBot/service/TelegramBot.java
@@ -0,0 +1,158 @@
+package com.project.TelegramBot.service;
+
+import com.project.TelegramBot.config.BotConfig;
+import com.project.TelegramBot.model.User;
+import com.project.TelegramBot.model.UserRepository;
+import com.vdurmont.emoji.EmojiParser;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    final BotConfig config;
+
+    static final String HELP_TEXT = "This bot is created to implement different features i came up with \n\n" +
+            "no features yet :(";
+
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+        List<BotCommand> listOfCommands = new ArrayList<>();
+        listOfCommands.add(new BotCommand("/start", "get a welcome message"));
+        listOfCommands.add(new BotCommand("/mydata", "get your data stored"));
+        listOfCommands.add(new BotCommand("/deletedata", "delete your data"));
+        listOfCommands.add(new BotCommand("/help", "info about the bot"));
+        listOfCommands.add(new BotCommand("/settings", "set your preferences"));
+        try {
+            execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        if(update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+
+
+            switch (messageText) {
+                case "/start":
+
+                    registerUser(update.getMessage());
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                case "/help":
+                    sendMessage(chatId, HELP_TEXT);
+                    break;
+                default:
+                    sendMessage(chatId, "Sorry, command not recognized");
+
+            }
+        }
+
+
+    }
+
+    private void registerUser(Message message) {
+
+        if(userRepository.findById(message.getChatId()).isEmpty()) {
+
+            Long chatId = message.getChatId();
+            Chat chat = message.getChat();
+
+            User user = new User();
+
+            user.setChatId(chatId);
+            user.setFirstName(chat.getFirstName());
+            user.setLastName(chat.getLastName());
+            user.setUsername(chat.getUserName());
+            user.setRegisteredAt(new Timestamp(System.currentTimeMillis()));
+
+            userRepository.save(user);
+
+
+        }
+
+
+    }
+
+    private void startCommandReceived(long chatId, String name){
+
+
+
+        String answer = "Hi, " + name + ", nice to meet you!" + EmojiParser.parseToUnicode(" :blush: ");
+
+        sendMessage(chatId, answer);
+
+    }
+
+    private void sendMessage(long chatId, String textToSend){
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+
+        List<KeyboardRow> keyboardRows = new ArrayList<>();
+        KeyboardRow row = new KeyboardRow();
+
+        row.add("weather");
+        row.add("joke");
+
+        keyboardRows.add(row);
+
+        row = new KeyboardRow();
+
+        row.add("register");
+        row.add("chack my data");
+        row.add("delete my data");
+
+        keyboardRows.add(row);
+
+        keyboardMarkup.setKeyboard(keyboardRows);
+
+        message.setReplyMarkup(keyboardMarkup);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getToken();
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+}
